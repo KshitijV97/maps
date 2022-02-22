a9884e65f9754118f3b6dd5a214988dffa51ed57
KshitijV97 <writetokshitij@gmail.com>
2022-02-22 23:03:19 +0530

    Removed unused imports

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,6 @@
 
 import { User } from './User';
 import { Company } from './Company';
-
-// const user = new User();
-// console.log(user);
-
-// const company = new Company();
-// console.log(company);
-
 import { CustomMap } from './CustomMap';
 
 const user = new User();
