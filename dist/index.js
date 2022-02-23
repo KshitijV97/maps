f84c8b9674d421ca685724472cb83f78617b4855
KshitijV97 <writetokshitij@gmail.com>
2022-02-23 18:07:26 +0530

    Adding other files

--- dist/index.js
+++ dist/index.js
@@ -212,7 +212,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "6889" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "10052" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
